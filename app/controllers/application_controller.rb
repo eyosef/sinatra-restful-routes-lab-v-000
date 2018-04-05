a13c659a4b2824afba4e38cd28fba5b9d174b632
Eden Mesfun <emesfun@gmail.com>
2018-04-05 12:57:13 +0000

    automatically backed up by learn

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -35,6 +35,7 @@ class ApplicationController < Sinatra::Base
 
   post '/recipes/:id' do
     @recipe = Recipe.find_by_id(params[:id])
+    binding.pry
     redirect to "/recipes/#{@recipe.id}"
   end
 
