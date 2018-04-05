c6b63777f3d1e715cba796b1ba85b739d1098309
Eden Mesfun <emesfun@gmail.com>
2018-04-05 13:41:05 +0000

    automatically backed up by learn

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -41,7 +41,7 @@ class ApplicationController < Sinatra::Base
 
   patch '/recipes/:id' do
     @recipe = Recipe.find_by(params[:name])
-    @recipe.update(:name => params[:name], :content => params[:content])
+    @recipe.update(:name => params[:name], :ingredients => params[:ingredients], :cook_time => params[:cook_time])
 
     redirect to ("/posts/#{@post.id}")
   end
