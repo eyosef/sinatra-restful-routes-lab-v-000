c03e4586275c776b0d2d444100e078866d3608d6
Eden Mesfun <emesfun@gmail.com>
2018-04-05 13:18:12 +0000

    automatically backed up by learn

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -39,7 +39,7 @@ class ApplicationController < Sinatra::Base
   end
 
   get '/recipes/<%=@recipe.id%>/delete' do
-    
+    @recipe = Recipe.find_by_id(params[:id])
     binding.pry
     erb :'deleted'
   end
